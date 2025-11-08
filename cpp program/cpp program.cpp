ac5618e553289296e91a13260a46aa5beb6a5692
Windowskey02 <windowskey02@gmail.com>
2025-11-08 10:28:53 +0200

    Added checks to see if any if the values is equal to 0.

--- cpp program/cpp program.cpp
+++ cpp program/cpp program.cpp
@@ -5,13 +5,25 @@ int main() {
 	int y;
 	cout << "Enter a number (x): " << endl;
 	cin >> x;
-	cout << "Enter another number (y): " << endl;
+	if (x == 0) {
+		cout << "Don't do that." << endl;
+		return 1;
+	}
+	else {
+		cout << "Enter another number (y): " << endl;
+	}
 	cin >> y;
-	cout << "Results: " << endl;
-	cout << "Addition: " << x + y << endl;
-	cout << "Subtraction: " << x - y << endl;
-	cout << "Multiplication: " << x * y << endl;
-	cout << "Division (x / y): " << x / y << endl;
-	cout << "Division (y / x): " << y / x << endl;
-	return 0;
+	if (y == 0) {
+		cout << "Don't do that." << endl;
+		return 1;
+	}
+	else {
+		cout << "Results: " << endl;
+		cout << "Addition: " << x + y << endl;
+		cout << "Subtraction: " << x - y << endl;
+		cout << "Multiplication: " << x * y << endl;
+		cout << "Division (x / y): " << x / y << endl;
+		cout << "Division (y / x): " << y / x << endl;
+		return 0;
+	}
 }
